951e062e4d562ffb5ecee23f201792870c44b5b7
stormsidali2001 <assoulsidali@gmail.com>
2022-07-19 20:39:12 +0200

    ...

--- fighting game/canvas.js
+++ fighting game/canvas.js
@@ -6,6 +6,19 @@ const {width,height} = canvas;
 ctx = canvas.getContext('2d');
 
 const player = new Sprite({
-    x:0,
-    y:0
+   position:{
+        x:0,
+        y:0
+   },
+   velocity:{
+        x:1,
+        y:1
+   }
 });
+
+function animate(){
+    ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+    player.update(ctx);
+    requestAnimationFrame(animate);
+}
+animate();
--- fighting game/sprite.js
+++ fighting game/sprite.js
@@ -1,5 +1,19 @@
 class Sprite{
-    constructor(position){
+    constructor({position,velocity,width,height}){
         this.position = position;
+        this.velocity = velocity;
+        this.width = width;
+        this.height = height;
+    }
+    update(ctx){
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.draw(ctx);
+    }
+    draw(ctx){
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
+        ctx.fill();
     }
 }
