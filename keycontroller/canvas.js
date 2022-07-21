90c3170a859ada585a5becb2ff8839b8770cdfe5
stormsidali2001 <assoulsidali@gmail.com>
2022-07-21 11:39:36 +0200

    ....

--- keycontroller/canvas.js
+++ keycontroller/canvas.js
@@ -6,16 +6,22 @@ canvas.height = window.innerHeight;
 const {width,height} = canvas;
 ctx = canvas.getContext('2d');
 
-
-
-let last = 0;
-let dt = 0;
-const speed = 64;
-let p1 = 0;
-let p2 = 0;
+const controls = new KeyControls();
+let x =0;
+let y = 0;
+let color = 0;
 function animate(ms){
-  
-
+    x+= controls.x;
+    y+= controls.y;
+  if(!controls.action){
+        color+= 10;
+        if(color >360){
+            color -= 360;
+        }
+  }
+  ctx.fillStyle = `hsl(${color}, 50% , 50%)`;
+  ctx.fillRect(x,y,50,50);
+ 
     requestAnimationFrame(animate);
 }
 animate();
--- keycontroller/key-controls.js
+++ keycontroller/key-controls.js
@@ -2,11 +2,11 @@ class KeyControls{
     constructor(){
         this.keys = {};
         document.addEventListener('keydown',e=>{
-            if(['ArrowLeft','ArrowRight','ArrowTop','ArrowDown'].find(e.key)){
+            if(['ArrowLeft','ArrowRight','ArrowTop','ArrowDown'].includes(e.key)){
                 e.preventDefault();// prevent scrolling when pressing the arrow keys
             }
-            
             this.keys[e.key] = true;
+            
         });
         document.addEventListener('keyup',e=>{
             this.keys[e.key] = false;
@@ -14,15 +14,16 @@ class KeyControls{
 
     }
     get action(){
-        return !!this.keys['space'];
+        return !!this.keys[' '];
     }
     get x(){
         if(this.keys['ArrowLeft'] || this.keys['a']) return -1;
         if (this.keys['ArrowRight'] || this.keys['d']) return 1;
-        
+        return 0;
     }
     get y(){
-        if(this.keys['ArrowUp'] || this.keys['w']) return 1;
-        if (this.keys['ArrowDown'] || this.keys['s']) return -1;
+        if(this.keys['ArrowUp'] || this.keys['w']) return -1;
+        if (this.keys['ArrowDown'] || this.keys['s']) return 1;
+        return 0;
     }
 }
